67044fa8df09569dae49e8d19cafc4749c849dd4
Yurij Mikhalevich <0@39.yt>
2017-03-19 19:10:16 +0300

    errors reporting improved

--- jdext.js
+++ jdext.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 exports.extract = (jdsave, webm)=>{
   const j = fs.readFileSync(jdsave);
   if (j[512495]!=0 || j[512496]!=0x1A)
-    throw new Error('cannot find JDSave video start byte, please report the issue here https://github.com/39dotyt/jdext/issues');
+    throw new Error('cannot find video start byte');
   const v = Buffer.from(j.buffer, 512496);
   fs.writeFileSync(webm, v);
 };
@@ -13,4 +13,7 @@ if (module.parent)
   return;
 if (process.argv.length < 4)
   return console.warn('not enought arguments\nexample: ./jdext.js JDSave_1 myvideo.webm');
-exports.extract(process.argv[2], process.argv[3]);
+try {
+  exports.extract(process.argv[2], process.argv[3]); }
+catch(e){
+  console.error(`${e.stack}\n\nplease copy log from above and report the issue here https://github.com/39dotyt/jdext/issues`);  }
